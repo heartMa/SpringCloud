b56ff1defff98c6c4017e78902ad438dc1bfe1ee
2zheteng <netheartma@gmail.com>
2019-07-17 13:23:03 +0800

    init

---
+++ 01-SpringCloud-Eureka-server/src/main/java/com/w3cjava/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//注解启动一个服务注册中心
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
