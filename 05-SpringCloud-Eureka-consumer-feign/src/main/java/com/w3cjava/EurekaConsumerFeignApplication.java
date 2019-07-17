e3611f5d572749d48c80b7702510dac25388cb8c
2zheteng <netheartma@gmail.com>
2019-07-17 14:24:46 +0800

    去除RestTemplate

--- 05-SpringCloud-Eureka-consumer-feign/src/main/java/com/w3cjava/EurekaConsumerFeignApplication.java
+++ 05-SpringCloud-Eureka-consumer-feign/src/main/java/com/w3cjava/EurekaConsumerFeignApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,10 +12,5 @@ import org.springframework.web.client.RestTemplate;
 public class EurekaConsumerFeignApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaConsumerFeignApplication.class, args);
-	}
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 }
