80d51ff5b9455af3f314071e6cf8ff50f5fa16db
2zheteng <netheartma@gmail.com>
2019-07-17 14:05:32 +0800

    ribbon

---
+++ 04-SpringCloud-Eureka-consumer-ribbon/src/main/java/com/w3cjava/EurekaConsumerRibbonApplication.java
@@ -0,0 +1,22 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaConsumerRibbonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumerRibbonApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
