865d8cd78671d166f46536da35f13dadc60a0b5c
2zheteng <netheartma@gmail.com>
2019-07-17 13:33:02 +0800

    consumer消费

--- 03-SpringCloud-Eureka-consumer/src/main/java/com/w3cjava/EurekaConsumerApplication.java
+++ 03-SpringCloud-Eureka-consumer/src/main/java/com/w3cjava/EurekaConsumerApplication.java
@@ -3,11 +3,18 @@ package com.w3cjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EurekaClientApplication {
+public class EurekaConsumerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
+		SpringApplication.run(EurekaConsumerApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- 03-SpringCloud-Eureka-consumer/src/main/java/com/w3cjava/controller/DcController.java
+++ 03-SpringCloud-Eureka-consumer/src/main/java/com/w3cjava/controller/DcController.java
@@ -1,21 +1,26 @@
 package com.w3cjava.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DcController {
 
-    @Autowired
-    DiscoveryClient discoveryClient;
+	@Autowired
+	LoadBalancerClient loadBalancerClient;
+	@Autowired
+	RestTemplate restTemplate;
 
-    @GetMapping("/dc")
-    public String dc() {
-        String services = "Services: " + discoveryClient.getServices();
-        System.out.println(services);
-        return services;
-    }
+	@GetMapping("/consumer")
+	public String dc() {
+		ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+		String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+		System.out.println(url);
+		return restTemplate.getForObject(url, String.class);
+	}
 
 }
