a14e5667b0d615138c67a7e7f31029fdeee584e2
2zheteng <netheartma@gmail.com>
2019-07-18 08:07:17 +0800

    config client git

---
+++ 09-SpringCloud-config-client-git/src/main/java/com/w3cjava/ConfigClientGitApplication.java
@@ -0,0 +1,18 @@
+package com.w3cjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @class  ConfigClientGitApplication
+ * @version 1.0.0
+ * @author cos
+ * @desc   http://localhost:2004/info
+ *
+ */
+@SpringBootApplication
+public class ConfigClientGitApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientGitApplication.class, args);
+	}
+}
