15dbbdd7ed5c4f6cb879580595e0f9b3b1b0f51a
2zheteng <netheartma@gmail.com>
2019-07-19 13:35:40 +0800

    消费组和分区实现

--- 17-SpringCloud-stream-hello/src/main/java/com/w3cjava/StreamHelloApplication.java
+++ 17-SpringCloud-stream-hello/src/main/java/com/w3cjava/StreamHelloApplication.java
@@ -10,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date   2019年7月19日
  * @desc   执行rabbitmq-plugins enable rabbitmq_management命令，开启Web管理插件，这样我们就可以通过浏览器来进行管理了
  * http://localhost:15672/
+ * application.properties配置
+ * spring.cloud.stream.bindings.input.group属性指定了该应用实例都属于Service-A消费组
+ * spring.cloud.stream.bindings.input.destination属性则指定了输入通道对应的主题名
  *
  */
 @SpringBootApplication
